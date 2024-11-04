7cc3f32f27a8c1a5d0e5569ff5ed51048f49f951
Tariq Yunusa <tariikhyunusa@gmail.com>
2024-11-04 19:59:47 +0100

    initial commit

---
+++ src/threeScene.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useRef } from 'react'
+import * as THREE from 'three'
+
+
+const threeScene = () => {
+  const mountRef = useRef(null)
+
+  useEffect(() => {
+    
+  },[])
+
+  return  <div ref={mountRef}/>
+  
+}
+
+export default threeScene
