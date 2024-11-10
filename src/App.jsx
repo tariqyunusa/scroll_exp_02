dd06de14f4cd66b1255bf617db71c55bbd2bed50
Tariq Yunusa <tariikhyunusa@gmail.com>
2024-11-10 13:41:26 +0100

    added title scroller

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import ThreeScene from './threeScene'
 
 function App() {
+  const [data, setData] = useState([])
 
+  const setter = async () => {
+    const movies = await fetchMovies()
+    setData(movies)
+  }
+
+  useEffect(() => {
+    setter()
+  }, [])
+
+  useEffect(() => {
+    console.log("movies", data)
+  }, [data]) 
 
   return (
     <>
-      <div style={{width: "100vw", height: "100vh", }}>
-    <ThreeScene />
+      <div style={{width: "90vw", height: "100vh", display: "flex", justifyContent: "space-between", alignItems: "center", position:"relative"}}>
+       
+        <ThreeScene data={data} />
+        <div className='title__slider'>{data && data.map((title, index) => (
+          <h3 key={index}>{title.title}</h3>
+        ))}</div>
       </div>
     </>
   )
 }
 
 export default App
+
+const fetchMovies = async () => {
+  const accessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMmQyZWEzM2Y3MjBhMTQ5MWU4MjE1MDM5MWEwNzYxZiIsIm5iZiI6MTczMTE0Njg0NC42NjE4MzUsInN1YiI6IjY1MjE1OTAzYzFmZmJkMDBmZTEwY2NmZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1HGu69GzirQ6feFPTbec7aJUyaIhh5xaKNcymg49GPE';
+  const url = 'https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1';
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  const data = await response.json();
+  return data.results.map(movie => ({
+    title: movie.title,
+    posterPath: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+  }));
+};
--- src/threeScene.jsx
+++ src/threeScene.jsx
@@ -1,20 +1,19 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 import { vertex } from './shaders/vertex';
 import { fragment } from './shaders/fragment';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import VirtualScroll from 'virtual-scroll';
-import * as dat from 'dat.gui';
 
 gsap.registerPlugin(ScrollTrigger);
 
-const ThreeScene = () => {
+const ThreeScene = ({ data }) => {
   const mountRef = useRef(null);
   const meshRefs = useRef([]);
   const scrollTarget = useRef(0);
   const scrollCurrent = useRef(0);
-  const cameraPosition = useRef({ x: 3, y: 0, z: 0 });
+  const cameraPosition = useRef({ x: 2, y: 0, z: 3 });
   const raycaster = new THREE.Raycaster();
   const mouse = new THREE.Vector2();
   const targetPositionY = useRef([]);
@@ -52,21 +51,6 @@ const ThreeScene = () => {
       targetPositionY.current[i] = 0;
       return container;
     });
-
-    // const gui = new dat.GUI();
-    raycaster.precision = 0.0001;
-
-    // const cameraFolder = gui.addFolder('Camera Position');
-    // cameraFolder.add(cameraPosition.current, 'x', -10, 10).onChange(value => {
-    //   camera.position.x = value;
-    // });
-    // cameraFolder.add(cameraPosition.current, 'y', -10, 10).onChange(value => {
-    //   camera.position.y = value;
-    // });
-    // cameraFolder.add(cameraPosition.current, 'z', -10, 10).onChange(value => {
-    //   camera.position.z = value;
-    // });
-    // cameraFolder.open();
 
     const scroller = new VirtualScroll();
     scroller.on(event => {
@@ -133,41 +117,18 @@ const ThreeScene = () => {
       window.removeEventListener('resize', handleResize);
       window.removeEventListener('mousemove', handleMouseMove);
       renderer.dispose();
-      gui.destroy();
     };
   };
 
   useEffect(() => {
-    // Fetch posters and initialize scene
-    const fetchPostersAndInitialize = async () => {
-      const movies = await fetchMovies();
+    if (data && data.length) {
       const textureLoader = new THREE.TextureLoader();
-      const loadedTextures = movies.map(movie => textureLoader.load(movie.posterPath));
-      
-      initializeScene(loadedTextures); 
-    };
+      const loadedTextures = data.map(movie => textureLoader.load(movie.posterPath));
+      initializeScene(loadedTextures);
+    }
+  }, [data]);
 
-    fetchPostersAndInitialize();
-  }, []); 
-
-  return <div ref={mountRef} style={{ width: '100vw', height: '100vh' }} />;
-};
-
-const fetchMovies = async () => {
-  const accessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMmQyZWEzM2Y3MjBhMTQ5MWU4MjE1MDM5MWEwNzYxZiIsIm5iZiI6MTczMTE0Njg0NC42NjE4MzUsInN1YiI6IjY1MjE1OTAzYzFmZmJkMDBmZTEwY2NmZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1HGu69GzirQ6feFPTbec7aJUyaIhh5xaKNcymg49GPE';
-  const url = 'https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1';
-  const response = await fetch(url, {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
-  const data = await response.json();
-  return data.results.map(movie => ({
-    title: movie.title,
-    posterPath: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
-  }));
+  return <div ref={mountRef} style={{ width: '90%', height: '100vh' }} />;
 };
 
 export default ThreeScene;
